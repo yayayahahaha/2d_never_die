93a18476fbf94afcbf90c963e969882c5e29436c
fg <fg@sysadmde-MacBook-Pro.local>
2016-12-30 16:33:14 +0800

    use nodejs get encode href done

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
-console.log("Hello, Welcome to PhantomJS");
-var cheerio = require('cheerio');
+ var request = require("request");
+ var fs = require("fs");
+ var cheerio = require("cheerio");
 
-var page = new WebPage();
-page.onConsoleMessage = function(msg) {
-	console.log(msg);
-};
+ var url = "http://2d-gate.org/thread-11294-1-1.html";
 
-var url = "https://www.google.com.tw";
-url = "http://2d-gate.org/thread-11294-1-1.html";
 
-var encodeHref = [];
-
-page.open(url, function(status, another) {
-	if (status === "success") {
-		var $ = cheerio.load(page.content);
-		var spans = $("span");
-		for (var i = 0; i < spans.length; i++) {
-			if ($(spans)[i].attribs.href) {
-				encodeHref.push($(spans)[i].attribs.href);
-			}
-		}
-		page.close();
-		afterGetEncodeHref();
-	} else {
-		console.log("page open failed!");
-	}
-});
-
-function afterGetEncodeHref() {
-	console.log("========================================================================");
-	console.log("after get encode href");
-	console.log("========================================================================");
-
-	var page2 = new WebPage();
-	page2.open("https://www.google.com.tw", function(res) {
-		console.log(res);
-		page2.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
-			console.log($);
-		});
-	});
-}
+ request({
+ 	url: url,
+ 	method: "GET"
+ }, function(e, r, b) {
+ 	if (e || !b) {
+ 		console.log("connect error!!!");
+ 		return;
+ 	}
+ 	var $ = cheerio.load(b);
+ 	var result = [];
+ 	var spans = $("span");
+ 	for (var i = 0; i < spans.length; i++) {
+ 		if ($(spans[i]).attr("href")) {
+ 			result.push($(spans[i]).attr("href"));
+ 		}
+ 	}
+ 	fs.writeFileSync("result.json", JSON.stringify(result));
+ });
