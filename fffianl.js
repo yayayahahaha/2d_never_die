822cd4d1a2eeca90afd00102e5f50dd2b9a05a53
fg <fg@sysadmde-MacBook-Pro.local>
2016-12-30 19:10:02 +0800

    2d-gate download program done

--- fffianl.js
+++ fffianl.js
@@ -1,6 +1,7 @@
  var request = require("request");
  var fs = require("fs");
  var cheerio = require("cheerio");
+ var http = require('https');
 
  var url = "https://demo-project-flyc-2.c9.io/php/finalArray";
 
@@ -16,7 +17,42 @@
 
  	var json = JSON.parse(b);
 
- 	console.log(json[0]);
+ 	// console.log(json[0]);
+
+ 	var download = function(uri, filename, callback) {
+ 		request.head(uri, function(err, res, body) {
+ 			if (err) {
+ 				callback(err, filename)
+ 				console.log("error");
+ 			} else {
+ 				console.log("ffffffff");
+ 				var stream = request(uri);
+ 				stream.pipe(
+ 						fs.createWriteStream(filename)
+ 						.on('error', function(err) {
+ 							console.log("error");
+ 							callback(error, filename);
+ 							stream.read();
+ 						})
+ 					)
+ 					.on('close', function() {
+ 						console.log("ffffffff");
+ 						callback(null, filename);
+ 					});
+ 			}
+ 		});
+ 	};
+
+ 	for (var i = 0; i < 1; i++) {
+ 		// console.log(json[i].link);
+ 		
+
+ 		download('https://cdn.sstatic.net/Sites/stackoverflow/img/sprites.svg?v=8c1c8cba242e', "0.svg", function() {
+ 			console.log("done");
+ 		});
+ 	}
+
+
 
  });
 
