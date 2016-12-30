a295b5af4ca6110faebfb46d0a8060711d75c7fa
fg <fg@sysadmde-MacBook-Pro.local>
2016-12-30 17:02:34 +0800

    make each js code for decoding in result.json

--- index.js
+++ index.js
@@ -5,28 +5,6 @@
  var url = "http://2d-gate.org/thread-11294-1-1.html";
 
 
- /* request({
-  	url: url,
-  	method: "GET"
-  }, function(e, r, b) {
-  	if (e || !b) {
-  		console.log("connect error!!!");
-  		return;
-  	}
-  	var $ = cheerio.load(b);
-  	var result = [];
-  	var spans = $("span");
-  	for (var i = 0; i < spans.length; i++) {
-  		if ($(spans[i]).attr("href")) {
-  			result.push($(spans[i]).attr("href"));
-  		}
-  	}
-  	fs.writeFileSync("result.json", JSON.stringify(result));
-  });*/
-
- // one
- url = "http://embed.2d-gate.org/embed/tcAotDagKBccuK44IZ2mxIa64yMGuCR2phaXk5qTYEOF2BRMYTHwGDFzYvcHP2gmJ5rlYjyeqS-amiX2Sg6TCw/?hash=fNCgG6odLvt2p1S0H5DJnv3HE9QgmuV06RBG7z_R3JprS9r_2z3qviPFowPPH5ZWZeem2nNQSNlxeBaQbpq7DPU8di-WPizik7neOqkaZlM";
-
  request({
  	url: url,
  	method: "GET"
@@ -37,7 +15,47 @@
  	}
  	var $ = cheerio.load(b);
  	var result = [];
- 	var scripts = $("script");
- 	var script = $(scripts)[8].children[0].data;
- 	fs.writeFileSync("result.json", script);
+ 	var spans = $("span");
+ 	for (var i = 0; i < spans.length; i++) {
+ 		if ($(spans[i]).attr("href")) {
+ 			result.push($(spans[i]).attr("href"));
+ 		}
+ 	}
+
+ 	// 現在resutl 裡面有這部動畫的所有尚未解密的連結
+ 	var javascriptS = [],
+ 		now = 0;
+ 	for (var i = 0; i < result.length; i++) {
+ 		request({
+ 			url: result[i],
+ 			method: "GET"
+ 		}, function(e, r, b) {
+ 			if (e || !b) {
+ 				console.log("connect error!!!");
+ 				return;
+ 			}
+ 			var $ = cheerio.load(b);
+ 			var scripts = $("script");
+ 			var script = $(scripts)[8].children[0].data;
+ 			console.log(script);
+ 			javascriptS.push(script);
+
+ 			jsAjaxfinished(result.length, javascriptS);
+
+ 		});
+ 	}
+ 	// fs.writeFileSync("result.json", script);
+
+ 	function jsAjaxfinished(total, javascriptS) {
+ 		now++;
+ 		console.log((now * 100 / total).toFixed(2) + "%");
+ 		if (now == total) {
+ 			console.log(javascriptS);
+ 			fs.writeFileSync("result.json", JSON.stringify(javascriptS));
+ 		}
+ 	}
+
  });
+
+
+ // fs.writeFileSync("result.json", JSON.stringify(result));
